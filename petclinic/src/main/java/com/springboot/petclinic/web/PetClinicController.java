cc4f0b641bb6faa5edab33cddc9050427488eef4
cem alpay <hcalpay@fonetyazilim.com>
2022-01-06 11:21:27 +0300

    add jsp

--- petclinic/src/main/java/com/springboot/petclinic/web/PetClinicController.java
+++ petclinic/src/main/java/com/springboot/petclinic/web/PetClinicController.java
@@ -1,16 +1,35 @@
 package com.springboot.petclinic.web;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springboot.petclinic.service.PetClinicService;
 
 @Controller
 public class PetClinicController {
 
-	@RequestMapping("hello")
-	@ResponseBody
-	public String hello() {
+	@Autowired
+	public PetClinicService petClinicService;
+	
+	@RequestMapping("/owners")
+	public ModelAndView getOwners(){
 		
-		return "Hello World";
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("owners",petClinicService.findOwners());
+		mav.setViewName("owners");
+		return mav;
+	}
+	
+	@RequestMapping("hello")
+	public ModelAndView hello() {
+		ModelAndView view = new ModelAndView();
+		view.addObject("message","Hello World");
+		view.addObject("time",new Date());
+		view.setViewName("hello");
+		return view;
 	}
 }
